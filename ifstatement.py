e478552bab12be05399c97f298c6c9427fe73065
shital081998 <shitaldavargave196@gmail.com>
2025-07-06 23:21:38 +0530

    range() function

---
+++ ifstatement.py
@@ -0,0 +1,29 @@
+a=10
+b=20
+if b>a:
+    print("b is grater than a")
+    print("hello")
+
+#if-else
+a=20
+b=30
+if a>b:
+    print("a is grater than b")
+else:
+    print("b is greater than a")
+
+#elif
+a=10
+b=20
+if a>b:
+    print("a is grater")
+elif a==b:
+    print("a and b is equal")
+else:
+    print("b iss greater")
+
+# one line statement
+a=100
+b=200
+(print("a is grater") if a>b else print("b is greater"))
+
---
+++ range.py
@@ -0,0 +1,7 @@
+for i in range(1,11):
+    if i%2==1:
+        print(i)
+
+for i in range(0,51,5):
+    print(i)
+
